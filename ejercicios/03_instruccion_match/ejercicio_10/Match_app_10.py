5d145aa791cfa307d7c874fdcb1ca9a4a70de96e
Martin Escalante <martinlionel4351@gmail.com>
2023-07-13 12:05:52 -0300

    ej 10, match

--- ejercicios/03_instruccion_match/ejercicio_10/Match_app_10.py
+++ ejercicios/03_instruccion_match/ejercicio_10/Match_app_10.py
@@ -6,6 +6,9 @@ import customtkinter
 
 
 '''
+    nombre:Martin Lionel 
+    apellido:Escalante
+    ---
 Una agencia de viajes nos pide informar si hacemos viajes a lugares según la estación del año. 
 En caso de hacerlo mostrar un alert con el mensaje “Se viaja”, 
 caso contrario mostrar “No se viaja”. 
@@ -42,9 +45,35 @@ class App(customtkinter.CTk):
         
     
     def btn_informar_on_click(self):
-        pass
+        estacion = self.combobox_estaciones.get()
+        destino = self.combobox_destino.get()
             
-    
+        match(estacion):
+            case "Invierno":
+                match(destino):
+                    case "Bariloche":
+                        mensaje = "Se viaja"
+                    case _:
+                        mensaje = "No se viaja"
+            case "Verano":
+                match(destino):
+                    case "Mar del plata" | "Cataratas":
+                        mensaje = "Se viaja"
+                    case _:
+                        mensaje = "No se viaja"
+            case "Otoño":
+                match(destino):
+                    case _:
+                        mensaje = "Se viaja"
+            case "Primavera":
+                match(destino):
+                    case "Bariloche":
+                        mensaje = "No se viaja"
+                    case _:
+                        mensaje = "Se viaja"
+
+        alert("ej 10, match", mensaje) 
+
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
